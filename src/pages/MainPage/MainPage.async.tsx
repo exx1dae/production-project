98ef3b6e25644730745952981cd4e6d9ae145b28
exxidae <sarkis200349@gmail.com>
2024-06-26 14:24:22 +0300

    initial commit

---
+++ src/pages/MainPage/MainPage.async.tsx
@@ -0,0 +1,11 @@
+import { lazy } from "react";
+
+export const MainPageAsync = lazy(
+  () =>
+    new Promise((resolve) => {
+      setTimeout(() => {
+        // @ts-ignore
+        return resolve(import("./MainPage"));
+      }, 5000);
+    }),
+);
