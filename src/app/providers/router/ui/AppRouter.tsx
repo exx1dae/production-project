4e532409dca5ebd606b0430e1189388336c93a9c
exxidae <sarkis200349@gmail.com>
2024-07-16 13:21:55 +0300

    add Loader, ErrorBoundary and NotFoundPage

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,19 +1,18 @@
-import { Route, Routes } from "react-router-dom";
-import { Suspense } from "react";
-import { routeConfig } from "shared/config/routeConfig/routeConfig";
+import { Route, Routes } from 'react-router-dom';
+import { Suspense } from 'react';
+import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'widgets/PageLoader';
 
-export const AppRouter = () => {
-  return (
-    <Suspense fallback={<div>Loading</div>}>
-      <Routes>
-        {Object.values(routeConfig).map(({ element, path }) => (
-          <Route
-            key={path}
-            path={path}
-            element={<div className="page-wrapper">{element}</div>}
-          />
-        ))}
-      </Routes>
-    </Suspense>
-  );
-};
+export const AppRouter = () => (
+  <Suspense fallback={<PageLoader />}>
+    <Routes>
+      {Object.values(routeConfig).map(({ element, path }) => (
+        <Route
+          key={path}
+          path={path}
+          element={<div className="page-wrapper">{element}</div>}
+        />
+      ))}
+    </Routes>
+  </Suspense>
+);
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,9 +1,14 @@
 import { useTranslation } from 'react-i18next';
+import { BugButton } from 'app/providers/ErrorBoundary';
 
 const MainPage = () => {
   const { t } = useTranslation();
 
-  return <div>{t('Главная страница')}</div>;
+  return (
+    <div>
+      {t('Главная страница')}
+    </div>
+  );
 };
 
 export default MainPage;
