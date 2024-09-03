f65acdc5abcb59de1c0ee2787cce0bca18b0446e
exx11daez <sarkis200349@gmail.com>
2024-09-03 13:24:11 +0300

    fix dev plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -8,18 +8,26 @@ import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 export const buildPlugins = ({
   paths,
   isDev,
-}: BuildOptions): webpack.WebpackPluginInstance[] => [
-  new HTMLWebpackPlugin({
-    template: paths.html,
-  }),
-  new webpack.ProgressPlugin(),
-  new MiniCssExtractPlugin({
-    filename: 'css/[name].[contenthash:8].css',
-    chunkFilename: 'css/[name].[contenthash:8].css',
-  }),
-  new webpack.DefinePlugin({
-    DEV: JSON.stringify(isDev),
-  }),
-  new ReactRefreshPlugin(),
-  new BundleAnalyzerPlugin({ openAnalyzer: false }),
-];
+}: BuildOptions): webpack.WebpackPluginInstance[] => {
+  const plugins = [
+    new HTMLWebpackPlugin({
+      template: paths.html,
+    }),
+    new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css',
+    }),
+    new webpack.DefinePlugin({
+      DEV: JSON.stringify(isDev),
+    }),
+
+  ];
+
+  if (isDev) {
+    plugins.push(new ReactRefreshPlugin());
+    plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
+  }
+
+  return plugins;
+};
