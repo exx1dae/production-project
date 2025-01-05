93a33d5016a2772be6aa8843fddc881d8ae26ce3
exxidae <sarkis200349@gmail.com>
2025-01-05 12:59:39 +0300

    fix pre-commit, update screenshots & add npm concurrently

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -10,12 +10,24 @@ export function useTheme(): UseThemeResult {
   const { theme, setTheme } = useContext(ThemeContext);
 
   const toggleTheme = () => {
-    const newTheme: Theme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+    let newTheme: Theme;
+
+    switch (theme) {
+      case Theme.LIGHT:
+        newTheme = Theme.DARK;
+        break;
+      case Theme.DARK:
+        newTheme = Theme.LIGHT;
+        break;
+      default:
+        newTheme = Theme.LIGHT;
+    }
+
     setTheme?.(newTheme);
 
     document.body.className = newTheme;
     localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
   };
 
-  return { theme: theme ?? Theme.LIGHT, toggleTheme };
+  return { theme: theme || Theme.LIGHT, toggleTheme };
 }
